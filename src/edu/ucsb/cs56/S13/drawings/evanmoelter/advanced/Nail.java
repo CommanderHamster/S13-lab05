85c1084567f8d6ee1f25aefeb8fa5c9abea8aa2a
Evan Moelter <evanmoelter@umail.ucsb.edu>
2013-05-24 14:29:55 -0700

    done with step 9 and half of 10

---
+++ src/edu/ucsb/cs56/S13/drawings/evanmoelter/advanced/Nail.java
@@ -0,0 +1,78 @@
+package edu.ucsb.cs56.S13.drawings.evanmoelter.advanced;
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.geom.AffineTransform; // translation, rotation, scale
+import java.awt.Shape; // general class for shapes
+
+// all imports below this line needed if you are implementing Shape
+import java.awt.geom.Point2D; 
+import java.awt.geom.Line2D; 
+import java.awt.geom.Rectangle2D;
+import java.awt.Rectangle;
+import java.awt.geom.PathIterator;
+import java.awt.geom.AffineTransform;
+
+import edu.ucsb.cs56.S13.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.S13.drawings.utilities.GeneralPathWrapper;
+
+/**
+   A vector drawing of a construction nail that implements
+   the Shape interface, and so can be drawn, as well as
+   rotated, scaled, etc.
+      
+   @author Evan Moelter
+   @author Phill Conrad
+   @version for CS56, Spring '13, UCSB
+   
+*/
+public class Nail extends GeneralPathWrapper implements Shape
+{
+    /**
+       Constructor
+
+       @param x x coord of lower left corner of nail
+       @param y y coord of lower left corner of nail
+       @param width width of the nail
+       @param height of nail (including first story and second story)
+     */
+    public Nail(double x, double y, double width, double height)
+    {
+    
+        // Rather than having to scale at the end, we can just
+        // draw things the right way to begin with, using the
+        // x, y, width and height.   If you haven't already
+        // hard coded a particular drawing, this may be an easier
+        // way.
+        
+        // Remember that y goes DOWN the page,
+        // so we ADD to y to get a "lower" value on the screen
+        
+	Line2D.Double top = new Line2D.Double
+	    (x, y+height, x+width, y+height);
+
+        Line2D.Double left = 
+            new Line2D.Double (x + (width/3), y + height,
+                               x + (width/3), y + (height/10));
+                               
+        Line2D.Double right =
+            new Line2D.Double (x + (2*width/3), y + height,
+                               x + (2*width/3), y + (height/10));
+
+	Line2D.Double pointLeft =
+            new Line2D.Double (x + width/3, y + height/10, x + width/2, y);
+	
+	Line2D.Double pointLeft =
+	    new Line2D.Double (x + 2*width/3, y + height/10, x + 2*width/2, y);
+	
+        // put the whole nail together
+       
+        GeneralPath wholeNail = this.get();
+        wholeNail.append(top, false);
+        wholeNail.append(left, false);
+        wholeNail.append(right, false); 
+        wholeNail.append(pointLeft, false);
+        wholeNail.append(pointRight, false);
+
+	
+    }
+
+}
